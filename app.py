0b604dbb3840e593dbf36c0b00a2cff3f548f5e2
futurofortissimo <michelemerelli.8@gmail.com>
2024-10-16 13:58:27 +0200

    will I break it?

--- app.py
+++ app.py
@@ -31,9 +31,8 @@ def process_backup_csv_files():
         print(f"Processing file: {csv_file}")
         username = csv_file.split('\\')[-1].split('_')[0]
         if '/' in username:
-            username = csv_file.split('/')[-1]
-        if '_' in username:
-            username = username.split('_')[-1]
+            username = csv_file.split('/')[-1].split('_')[0]
+
 
         strava_link = 'https://www.strava.com/athletes/' + csv_file.split('_')[1]
 

cc1c3ff47db4e7fbe4ac6634610650f8008b47a5
futurofortissimo <michelemerelli.8@gmail.com>
2024-10-16 09:55:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def process_backup_csv_files():
         print(f"Processing file: {csv_file}")
         username = csv_file.split('\\')[-1].split('_')[0]
         if '/' in username:
-            username = csv_file.split('/')[-1]
+            username = username.split('/')[-1]
 
         strava_link = 'https://www.strava.com/athletes/' + csv_file.split('_')[1]
 

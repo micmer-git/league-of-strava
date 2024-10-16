9b022f05725dd33cc52b4ddb0de1f65cfa9ab536
futurofortissimo <michelemerelli.8@gmail.com>
2024-10-16 09:45:58 +0200

    mago italian?

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def process_backup_csv_files():
             try:
                 with open(csv_file, 'r', encoding=encoding) as file:
                     reader = csv.DictReader(file)
-                    print(csv_file)
                     username = csv_file.split('\\')[-1].split('_')[0]
                     strava_link = 'https://www.strava.com/athletes/' + csv_file.split('_')[1]
                     try:

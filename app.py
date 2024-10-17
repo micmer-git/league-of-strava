a831cbb43b929bf1e4dafbb277f0b3014d934223
futurofortissimo <michelemerelli.8@gmail.com>
2024-10-17 13:15:18 +0200

    ripristino e about.html

--- app.py
+++ app.py
@@ -48,11 +48,8 @@ app.config['INITIALIZATION_DONE'] = False
 # Initialize SQLAlchemy and Flask-Migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 # Database Models
 class User(db.Model):
-    __tablename__ = 'users'  # Explicitly set table name to avoid conflicts
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     strava_link = db.Column(db.String(200), unique=True, nullable=False)
@@ -62,8 +59,8 @@ class User(db.Model):
     coins_everest = db.Column(db.Float, nullable=False, default=0.0)
     coins_pizza = db.Column(db.Float, nullable=False, default=0.0)
     coins_heartbeat = db.Column(db.Integer, nullable=False, default=0)
-    achievements = db.Column(db.JSON, nullable=False, default=dict)
-    stats = db.Column(db.JSON, nullable=False, default=dict)
+    achievements = db.Column(db.JSON, nullable=False, default={})
+    stats = db.Column(db.JSON, nullable=False, default={})
     max_elevation = db.Column(db.Float, nullable=True, default=0.0)
     max_elevation_link = db.Column(db.String(200), nullable=True, default='#')
     max_duration = db.Column(db.Float, nullable=True, default=0.0)
@@ -85,8 +82,6 @@ class User(db.Model):
     activities = db.relationship('Activity', backref='user', lazy=True)
 
 class Activity(db.Model):
-    __tablename__ = 'activities'  # Explicitly set table name
-
     id = db.Column(db.Integer, primary_key=True)
     activity_id = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(200), nullable=False)
@@ -101,7 +96,7 @@ class Activity(db.Model):
     coins_pizza = db.Column(db.Float, nullable=False)
     coins_heartbeat = db.Column(db.Integer, nullable=False)
     link = db.Column(db.String(200), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     # New Field to Store Additional Data
     additional_data = db.Column(db.JSON, nullable=True)
@@ -1834,10 +1829,10 @@ def dashboard_search():
     return redirect(url_for('index'))
 
 image_assignments = {
-    'lacco': 'static/cards/Gemini_Generated_Image_k1tmr9k1tmr9k1tm.jpg',
-    'mago': 'static/cards/Gemini_Generated_Image_vzmsilvzmsilvzms.jpg',
-    'liuk': 'static/cards/Gemini_Generated_Image_kqrkiqkqrkiqkqrk.jpg',
-    'micmer': 'static/cards/Gemini_Generated_Image_118vyu118vyu118v.jpg',
+    'lacco': '/static/cards/Gemini_Generated_Image_k1tmr9k1tmr9k1tm.jpg',
+    'mago': '/static/cards/Gemini_Generated_Image_vzmsilvzmsilvzms.jpg',
+    'liuk': '/static/cards/Gemini_Generated_Image_kqrkiqkqrkiqkqrk.jpg',
+    'micmer': '/static/cards/Gemini_Generated_Image_118vyu118vyu118v.jpg',
     # Add more users as needed
 
 }
